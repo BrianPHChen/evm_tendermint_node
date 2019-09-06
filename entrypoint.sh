69240be9af15e66821ac2335f6c72e85330ee56c
pohanchen <pohanchen@pohanchens-MacBook-Air.local>
2019-09-06 11:25:03 +0800

    add node binfile and env config

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Setting evm-lite config
+
+mkdir -p /root/.evm-lite/eth
+mkdir -p /root/.evm-lite/tendermint
+cp pwd.txt /root/.evm-lite/eth/
+./geth account new --password pwd.txt --datadir "./"
+./tendermint init --home "/root/.evm-lite/tendermint"
+sed -i "s/create_empty_blocks = true/create_empty_blocks = false/g" /root/.evm-lite/tendermint/config/config.toml
+sed -i "4,18d" /root/.evm-lite/tendermint/config/genesis.json
+shopt -s nullglob
+for file in ./keystore/*; do
+    Account=`echo $file | rev | cut -c1-40 | rev`
+done
+shopt -u nullglob
+sed "s/<coinbase_account>/$Account/g" genesis.json.sample > genesis.json
+mv keystore/ /root/.evm-lite/eth/
+mv genesis.json /root/.evm-lite/eth/
